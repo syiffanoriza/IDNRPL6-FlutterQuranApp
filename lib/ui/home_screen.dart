adaf4b6d8fc4e6fefc43c28a706ee5dcdc756750
syiffanoriza <syiffa.noriza@gmail.com>
2024-01-29 14:16:13 +0700

    Day 3: KickStarting HomeScreen (AppBar)

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      appBar: _appBar(),
+      backgroundColor: Colors.white,
+      body: Text('Home Screen'),
     );
   }
+
+  AppBar _appBar() => AppBar(
+    backgroundColor: Colors.white,
+    title: Row(
+      children: [
+        IconButton(
+            onPressed: (() => {}),
+            icon: SvgPicture.asset('/svg/menu_icon.svg')
+        ),
+      ],
+    ),
+  );
 }
