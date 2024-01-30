259dc3a38ee74d04a2c26ef1a2ce60fdd233cace
syiffanoriza <syiffa.noriza@gmail.com>
2024-01-30 08:12:23 +0700

    Day 4: Finishing Up Appbar

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quranapp/global.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,12 +17,28 @@ class HomeScreen extends StatelessWidget {
 
   AppBar _appBar() => AppBar(
     backgroundColor: Colors.white,
+    automaticallyImplyLeading: false,
     title: Row(
       children: [
         IconButton(
             onPressed: (() => {}),
-            icon: SvgPicture.asset('/svg/menu_icon.svg')
+            icon: SvgPicture.asset('/svg/menu.svg')
         ),
+        const SizedBox(
+          width: 24,
+        ),
+        Text(
+          'Quran App',
+          style: GoogleFonts.poppins(
+            color: primary,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Spacer(),
+        IconButton(
+            onPressed: (() => {}),
+            icon: SvgPicture.asset('/svg/search.svg'))
       ],
     ),
   );
